8e73d3dfa60d910a6dc8526a04a5d702a040c1fa
ManojkumarS9543 <mano22056.it@rmkec.ac.in>
2024-08-11 14:45:26 +0530

    Date Time

--- Add.java
+++ Add.java
@@ -4,12 +4,9 @@ import java.time.*;
 public class Add {
      public static void main(String args[]){
         System.out.println("Lets Learn java");
-         LocalDate thisTime = LocalDate.from(LocalDateTime.from(LocalTime.of(12,12)));
-         LocalDate nextTime = LocalDate.from(LocalDateTime.from(LocalTime.of(21,12)));
-         Period a = Period.between(thisTime,nextTime);
-         LocalDate someDay = LocalDate.now();
-         LocalDateTime toDay = someDay.atTime(LocalTime.from(thisTime));
-         LocalTime t = LocalTime.of(11,11);
-        System.out.println(thisTime);
+         LocalTime a = LocalTime.now();
+         LocalDate b = LocalDate.now();
+         LocalDateTime c = b.atTime(a);
+        System.out.println(c);
     }
 }
