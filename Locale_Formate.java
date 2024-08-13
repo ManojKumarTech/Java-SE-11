33d972d69c41567ec222da5fb77115c1c6175a52
ManojkumarS9543 <mano22056.it@rmkec.ac.in>
2024-08-13 21:24:38 +0530

    Locale_Formate

--- Locale_Formate.java
+++ Locale_Formate.java
@@ -1,12 +1,20 @@
 import java.math.BigDecimal;
+import java.text.NumberFormat;
 import java.util.Locale;
 
-public class Locale_Formate{
-    public static void main(String args[]){
-        Locale current = Locale.getDefault();
-        //thaiLand calender:)
+public class Locale_Formate {
+    public static void main(String args[]) {
+        Locale uk = new Locale("en", "GB");
+        // Thai calendar locale
         Locale th = Locale.forLanguageTag("th-TH-u-ca-buddhist-nu-thai");
         System.out.println(th);
 
+        BigDecimal prize = BigDecimal.valueOf(2.99);
+        double tax = 0.2;
+        int quantity = 12345;
+
+        NumberFormat currencyFormat = NumberFormat.getCurrencyInstance(uk);
+        String fPrize = currencyFormat.format(prize); // Format the BigDecimal value, not the Locale object
+        System.out.println(fPrize);
     }
 }
