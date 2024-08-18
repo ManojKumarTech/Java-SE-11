f7cd74d07619c84bb8ddf6b988c95da0dbc8dfdf
ManojkumarS9543 <mano22056.it@rmkec.ac.in>
2024-08-18 22:02:46 +0530

    Oops Concept

--- OopsConcept/Method.java
+++ OopsConcept/Method.java
@@ -1,4 +1,34 @@
 package OopsConcept;
 
 public class Method {
+    public static void main(String[] args) {
+        add1();
+        add2(1,2);
+        System.out.println(add3());
+        System.out.println(add4(3,7));
+    }
+
+    public static void add1(){
+        int a = 10;
+        int b = 12;
+        int c = a+b;
+        System.out.println(c);
+    }
+
+    public static void add2(int a,int b){
+        int c = a+b;
+        System.out.println(c);
+    }
+
+    public static int add3(){
+        int a = 15;
+        int b = 20;
+        int c = a+b;
+        return c;
+    }
+
+    public static int add4(int a,int b){
+        int c = a+b;
+        return c;
+    }
 }
