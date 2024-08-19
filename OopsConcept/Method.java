53c0e429b4d14a505d5830bb857267770177a00b
ManojkumarS9543 <mano22056.it@rmkec.ac.in>
2024-08-19 11:49:41 +0530

    Oops Concept

--- OopsConcept/Method.java
+++ OopsConcept/Method.java
@@ -1,6 +1,7 @@
 package OopsConcept;
 
 public class Method {
+    //Main Method
     public static void main(String[] args) {
         add1();
         add2(1,2);
@@ -8,6 +9,7 @@ public class Method {
         System.out.println(add4(3,7));
     }
 
+    //    Non-returnType & Non-Parameterised
     public static void add1(){
         int a = 10;
         int b = 12;
@@ -15,11 +17,13 @@ public class Method {
         System.out.println(c);
     }
 
+    //    Non-returnType & Parameterised
     public static void add2(int a,int b){
         int c = a+b;
         System.out.println(c);
     }
 
+    //    returnType & Non-Parameterised
     public static int add3(){
         int a = 15;
         int b = 20;
@@ -27,6 +31,7 @@ public class Method {
         return c;
     }
 
+    //    returnType & Parameterised
     public static int add4(int a,int b){
         int c = a+b;
         return c;
---
+++ OopsConcept/Recurtion.java
@@ -0,0 +1,34 @@
+package OopsConcept;
+
+public class Recurtion {
+
+    public static void main(String[] args) {
+        recursion1(7);
+        fac(3);
+    }
+
+    public static void recursion1(int a){
+        //Base Case
+        if(a==1){
+            System.out.println(a);
+        }
+
+        //Recursive Case
+        else{
+            System.out.println(a);
+            recursion1(a-1);
+        }
+    }
+    public static int r = 1;
+    public static void fac(int a) {
+
+        if (a == 1) {
+            System.out.println(r);
+        }
+        else{
+            r=a*r;
+            fac(a-1);
+        }
+    }
+
+}
