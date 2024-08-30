70da7d04d307b9ad4ab2f897769f69a519c56c9d
ManojkumarS9543 <mano22056.it@rmkec.ac.in>
2024-08-30 09:49:13 +0530

    Fibonacci

--- OopsConcept/Recursion.java
+++ OopsConcept/Recursion.java
@@ -5,6 +5,8 @@ public class Recursion {
     public static void main(String[] args) {
         recursion1(7);
         fac(3);
+        show();
+        fib(10);
     }
 
     public static void recursion1(int a){
@@ -29,6 +31,19 @@ public class Recursion {
             r=a*r;
             fac(a-1);
         }
+    }
+    static int n1=0,n2=1,n3=0;
+    public static void fib(int a){
+        if(a>0){
+            n3=n1+n2;
+            n1=n2;
+            n2=n3;
+            System.out.print(n3+" ");
+            fib(a-1);
+        }
+    }
+    public static void show(){
+        System.out.print(0+" "+1+" ");
     }
 
 }
