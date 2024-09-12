80ac1e4398baf72dcc75d31e332ac7ee0816566e
ManojkumarS9543 <mano22056.it@rmkec.ac.in>
2024-09-12 23:13:30 +0530

    MethodOverLoading

---
+++ Inheritance/MethodOverLoadind.java
@@ -0,0 +1,28 @@
+package Inheritance;
+
+public class MethodOverLoadind {
+    public static void main(String[] args) {
+        Method mObj = new Method();
+        mObj.min(10,100);
+        mObj.min(10,50,2);
+    }
+}
+class Method{
+    void min(int a,int b){
+        if(a>b){
+            System.out.println(b);
+        }else {
+            System.out.println(a);
+        }
+    }
+    void min(int a,int b,int c){
+        if(a>b && c>b){
+            System.out.println(b);
+        }else if(b>a && c>a){
+            System.out.println(a);
+        }else{
+            System.out.println(c);
+        }
+    }
+}
+
