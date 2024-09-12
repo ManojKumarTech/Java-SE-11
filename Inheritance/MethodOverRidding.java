781270d0253604ddd5384d063c1467682e4ed506
ManojkumarS9543 <mano22056.it@rmkec.ac.in>
2024-09-12 23:22:56 +0530

    MethodOverRidding

---
+++ Inheritance/MethodOverRidding.java
@@ -0,0 +1,29 @@
+package Inheritance;
+
+public class MethodOverRidding {
+    public static void main(String args[]) {
+        Dogs dObj = new Dogs();
+        System.out.println(dObj.canBark);
+        dObj.eat();
+        dObj.walk();
+
+    }
+    }
+class Animal{
+    int noLegs = 4;
+    public static void eat(){
+        System.out.println("I can eat");
+    }
+    public static void walk(){
+        System.out.println("I can Walk");
+    }
+}
+
+class Dogs extends Animals{
+    boolean canBark = true;
+    int noLegs = 5;
+    public static void eat() {
+        System.out.println("I can't eat");
+    }
+
+}
