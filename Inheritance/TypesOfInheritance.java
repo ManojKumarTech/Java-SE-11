35f53d3487a18a9b1856bd74f645d7da4aff6c19
ManojkumarS9543 <mano22056.it@rmkec.ac.in>
2024-09-17 20:32:55 +0530

    Inheritance

--- Inheritance/TypesOfInheritance.java
+++ Inheritance/TypesOfInheritance.java
@@ -1,16 +1,28 @@
 package Inheritance;
 
+import java.sql.SQLOutput;
+
 public class TypesOfInheritance {
     public static void main(String[] args) {
+        GrandFather GH = new GrandDaughter();
+        GH.print();
+        Mother M = new Mother();
+        M.print();
+        Uncle U = new Uncle();
+        U.print();
         Daughter s = new Daughter();
         s.print();
+        GrandDaughter GD = new GrandDaughter();
+        GD.print();
+
     }
 }
 
 class GrandFather{
     char gender = 'M';
     void print(){
-        System.out.println(gender);
+        System.out.println("Iam GrandFather");
+        System.out.println("Gender:"+gender);
     }
 }
 
@@ -18,11 +30,31 @@ class GrandFather{
 class Mother extends GrandFather{
       char gender = 'F';
       void print(){
-          System.out.println(gender);
+          System.out.println("Iam Mother");
+          System.out.println("Gender:"+gender);
       }
 }
 
+
 //Multi-level Inheritance
 class Daughter extends Mother{
+     void print(){
+         System.out.println("Iam Daughter of Mother Class");
+         System.out.println("Gender:"+gender);
+     }
+}
 
+//Hybrid Inheritance
+class Uncle extends GrandFather{
+    void print(){
+        System.out.println("Iam GrandFather's son(Uncle class) ");
+        System.out.println("Gender:"+gender);
+    }
+}
+//Hierarchical [Hybrid + MultiLevel]
+class GrandDaughter extends Daughter{
+    void print(){
+        System.out.println("Iam GrandDaughter Daughter class's Daughter ");
+        System.out.println("Gender:"+gender);
+    }
 }
